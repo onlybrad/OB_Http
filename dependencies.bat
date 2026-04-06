f8c53eb988ed18b79d80359505eaa299ed217283
OnlyBrad <bradonly420@gmail.com>
2026-04-06 11:24:48 -0400

    added includes folder and modified dependencies.bat to use all the cpu cores

--- dependencies.bat
+++ dependencies.bat
@@ -2,7 +2,7 @@ REM build libcurl (dynamic)
 mkdir vendors\libcurl\build\debug
 cd vendors\libcurl\build\debug
 cmake ..\.. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DENABLE_DEBUG=ON -DUSE_SCHANNEL=ON -DUSE_WINDOWS_SSPI=ON
-cmake --build . --config Debug
+cmake --build . --config Debug -- -j%NUMBER_OF_PROCESSORS%
 copy lib\libcurl-d.dll ..\..\..\..\
 cd ..\..\..\..
 
