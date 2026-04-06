0e3d88ba73ae0dbfce87386c838885ed2c5ef552
OnlyBrad <bradonly420@gmail.com>
2026-04-06 11:07:24 -0400

    added static library build

---
+++ build.debug.bat
@@ -0,0 +1,10 @@
+call dependencies.bat
+
+gcc -std=c99 -g -c src/*.c -Ivendors\libcurl\include -Ivendors\CJSON -Wall -Wextra -pedantic -Wconversion -Wstrict-overflow=5 -Wshadow -Wunused-macros -Wbad-function-cast -Wcast-qual -Wcast-align -Wwrite-strings -Wdangling-else -Wlogical-op -Wstrict-prototypes -Wold-style-definition -Wmissing-prototypes -Winline
+
+ar rcs obhttp.a
+for %%f in (*.o) do (
+    ar r obhttp.a %%f
+)
+
+del /Q *.o 2>nul
