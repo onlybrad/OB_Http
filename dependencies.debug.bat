f103b62a9aa5e730f1be6cd2d7ab0b3ea1d32c7f
OnlyBrad <bradonly420@gmail.com>
2026-04-16 12:54:12 -0400

    fixed dependencies.debug.bat

--- dependencies.debug.bat
+++ dependencies.debug.bat
@@ -1,8 +1,8 @@
 REM build libcurl debug (dynamic)
 mkdir vendors\libcurl\build\debug
 cd vendors\libcurl\build\debug
-cmake ..\.. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DUSE_SCHANNEL=ON -DUSE_WINDOWS_SSPI=ON
-cmake --build . --config Release -- -j%NUMBER_OF_PROCESSORS%
+cmake ..\.. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DUSE_SCHANNEL=ON -DUSE_WINDOWS_SSPI=ON
+cmake --build . --config Debug -- -j%NUMBER_OF_PROCESSORS%
 copy lib\libcurl.dll ..\..\..\..\
 cd ..\..\..\..
 
